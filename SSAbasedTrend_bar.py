f8f3893a37bae54d414dcbdbefccfcefd7a6f959
GoDK36 <siinwoo0306@gmail.com>
2020-02-04 17:18:39 +0900

    날짜별로 그룹화까지 최종 완성

--- SSAbasedTrend_bar.py
+++ SSAbasedTrend_bar.py
@@ -7,15 +7,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 import datetime as dt
 
-data = list(csv.reader(open(r'G:\Programming\python\창회선배스터디\SSAbasedTrend\1-SO-SSAbasedTrend-InputSample.t.csv', 'r', encoding='cp949')))[1:]
-pol_data = pd.read_csv(r'G:\Programming\python\창회선배스터디\SSAbasedTrend\1-SO-SSAbasedTrend-InputSample.t.csv', names=['date','pol'])[1:]
+data = list(csv.reader(open(r'C:\GOD\programming\NLP\DecoSentA\SSAbased_Trend\1-SO-SSAbasedTrend-InputSample.t.csv', 'r', encoding='cp949')))[1:]
+pol_data = pd.read_csv(r'C:\GOD\programming\NLP\DecoSentA\SSAbased_Trend\1-SO-SSAbasedTrend-InputSample.t.csv', names=['date','pol'])[1:]
 
 #data의 논항들 리스트화
 pv = list(set(pol_data.pol.tolist())) 
 
-###날짜 기간 설정
+##데이터 프레임 정렬
 
-n = int(input('days:'))
 startDate = data[0][0]
 endDate = data[-1][0] 
 tempDt =pd.to_datetime(startDate,dayfirst=True)
@@ -25,13 +24,12 @@ dtList = []
 while True:
     tempStr = str(tempDt)
     dtList.append(tempStr)
-    tempDt += dt.timedelta(days=n) ##이 숫자를 변형해서 주기 설정 가능 , 단, weeks(주)단위가 최대
+    tempDt += dt.timedelta(days=1) ##이 숫자를 변형해서 주기 설정 가능 , 단, weeks(주)단위가 최대
     if tempDt >= endDt:
         tempStr = str(tempDt)
         dtList.append(tempStr)
         break
 
-##값들별로 리스트화 하기
 res = []
 
 for x in data:
@@ -47,14 +45,17 @@ for x in data:
         else:
             continue
     res.append(temp)
-
-#데이터프레임화 한 후 sumup해주기
 pol_df = pd.DataFrame(res)
-pol_df = pol_df.fillna(0)
-pol_df = pol_df.groupby(['date'], as_index=False).sum() ##기준이되는 열로 병합해주기
-pol_df = pol_df[1:]
-pol_df = pol_df[['date','POSITIVE','NEGATIVE']]
-print(pol_df)
+pol_df = pol_df.groupby(['date'], as_index=False).sum()
+pol_df['date'] = pd.to_datetime(pol_df['date'], format='%Y-%m-%d')
+pol_df = pol_df.set_index('date', inplace=False)
+
+##날짜 범위 설정해주기
+n = input('숫자 + D(Days), W(Weeks), M(Month), Y(Years) (ex: 3일간격 = 3D): ')
+n_pol_df = pol_df.resample(n).sum()
+n_pol_df = n_pol_df.reset_index('date')
+print(n_pol_df)
+
 ##그래프 그리기
 
 plt.rcParams["font.size"] = 12
@@ -62,11 +63,11 @@ plt.rcParams["figure.figsize"] = (12, 8)
 
 plt.figure()
 
-x = np.arange(len(pol_df['date']))
+x = np.arange(len(n_pol_df['date']))
 
-plt.bar(x-0.0, pol_df['POSITIVE'], label='POSITIVE', width=0.3, color='blue')
-plt.bar(x+0.2,pol_df['NEGATIVE'] , label='NEGATIVE', width=0.3, color='red')
-plt.xticks(x, pol_df['date'])
+plt.bar(x-0.0, n_pol_df['POSITIVE'], label='POSITIVE', width=0.3, color='blue')
+plt.bar(x+0.2,n_pol_df['NEGATIVE'] , label='NEGATIVE', width=0.3, color='red')
+plt.xticks(x, n_pol_df['date'])
 
 plt.legend()
 plt.xlabel('date')
@@ -75,7 +76,7 @@ plt.title('')
 
 plt.show()
 
-##csv파일로 출력
-pol_df.to_csv(r'F:\Programming\python\창회선배스터디\SSAbasedTrend\result.csv')
+# ##csv파일로 출력
+# pol_df.to_csv(r'F:\Programming\python\창회선배스터디\SSAbasedTrend\result.csv')
 
-os.startfile(r'F:\Programming\python\창회선배스터디\SSAbasedTrend\result.csv')
+# os.startfile(r'F:\Programming\python\창회선배스터디\SSAbasedTrend\result.csv')
--- SSAbasedTrend_line.py
+++ SSAbasedTrend_line.py
@@ -7,15 +7,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 
-data = list(csv.reader(open(r'G:\Programming\python\NLP\DecoSentA\SSAbasedTrend\1-SO-SSAbasedTrend-InputSample.t.csv', 'r', encoding='cp949')))[1:]
-pol_data = pd.read_csv(r'G:\Programming\python\NLP\DecoSentA\SSAbasedTrend\1-SO-SSAbasedTrend-InputSample.t.csv', names=['date','pol'])[1:]
+data = list(csv.reader(open(r'C:\GOD\programming\NLP\DecoSentA\SSAbased_Trend\1-SO-SSAbasedTrend-InputSample.t.csv', 'r', encoding='cp949')))[1:]
+pol_data = pd.read_csv(r'C:\GOD\programming\NLP\DecoSentA\SSAbased_Trend\1-SO-SSAbasedTrend-InputSample.t.csv', names=['date','pol'])[1:]
 
 #data의 논항들 리스트화
 pv = list(set(pol_data.pol.tolist())) 
 
-###날짜 기간 설정
+##데이터 프레임 정렬
 
-n = int(input('days:'))
 startDate = data[0][0]
 endDate = data[-1][0] 
 tempDt =pd.to_datetime(startDate,dayfirst=True)
@@ -25,16 +24,13 @@ dtList = []
 while True:
     tempStr = str(tempDt)
     dtList.append(tempStr)
-    tempDt += dt.timedelta(days=n) ##이 숫자를 변형해서 주기 설정 가능 , 단, weeks(주)단위가 최대
+    tempDt += dt.timedelta(days=1) ##이 숫자를 변형해서 주기 설정 가능 , 단, weeks(주)단위가 최대
     if tempDt >= endDt:
         tempStr = str(tempDt)
         dtList.append(tempStr)
         break
 
-##값들별로 리스트화 하기
 res = []
-
-
 
 for x in data:
     temp = {}
@@ -49,33 +45,50 @@ for x in data:
         else:
             continue
     res.append(temp)
-
-#데이터프레임화 한 후 sumup해주기
 pol_df = pd.DataFrame(res)
-# pol_df = pol_df.fillna(0)
-pol_df = pol_df.groupby(['date'], as_index=False).sum() ##기준이되는 열로 병합해주기
-pol_df = pol_df[1:]
-pol_df = pol_df[['date','POSITIVE','NEGATIVE']]
-print(pol_df)
-##그래프 그리기
+pol_df = pol_df.groupby(['date'], as_index=False).sum()
+pol_df['date'] = pd.to_datetime(pol_df['date'])
+pol_df = pol_df.set_index('date', inplace=False)
 
-# smthlv=int(input('smooth level:'))
 
-# pol_graph = (
-#     ggplot(pol_df,aes(x='date'))
-#     + stat_smooth(aes(y='POSITIVE'),color='blue',geom='smooth', position='identity',method='lm',se=False,n=smthlv,group=1) #평균 선 그리기
-#     + stat_smooth(aes( y='NEGATIVE'),color='red',method='lm',se=False,n=smthlv,group=2) #평균 선 그리기
-#     + geom_point(aes(x='date', y='POSITIVE'),color='blue',alpha=0.3) #alpha는 투명도 0부터1까지
-#     + geom_line(aes(x='date', y='POSITIVE'),color='blue',alpha=0.3,group=1)
-#     + geom_point(aes(x='date', y='NEGATIVE'),color='red',alpha=0.3)
-#     + geom_line(aes(x='date', y='NEGATIVE'),color='red',alpha=0.3,group=2)
-#     + xlab('date')
-#     + ylab('Sentiment')
+##날짜 범위 설정해주기
+n = input('숫자 + D(Days), W(Weeks), M(Month), Y(Years) (ex: 3일간격 = 3D): ')
+n_pol_df = pol_df.resample(n).sum()
+n_pol_df = n_pol_df.reset_index('date')
+print(n_pol_df)
 
-# )
 
-# print(pol_graph)
-# ##csv파일로 출력
-# pol_df.to_csv(r'G:\Programming\python\NLP\DecoSentA\SSAbasedTrend\result.csv')
+#그래프 그리기
 
-# os.startfile(r'G:\Programming\python\NLP\DecoSentA\SSAbasedTrend\result.csv')
+pol_graph = (
+    ggplot(n_pol_df,aes(x='date'))
+    + stat_smooth(aes(y='POSITIVE', group=1),se=False,color='#33cccc') #평균 선 그리기
+    + stat_smooth(aes( y='NEGATIVE', group=2),color='crimson', se=False) #평균 선 그리기
+    + geom_line(aes(y='POSITIVE'),color='#33cccc',alpha=0.3)
+    + geom_line(aes(y='NEGATIVE'),color='crimson',alpha=0.3)
+    + geom_point(aes(y='POSITIVE'),color='#33cccc', size=2,alpha=0.3) #alpha는 투명도 0부터1까지
+    + geom_point(aes(y='NEGATIVE'),color='crimson', size=2,alpha=0.3)
+    + ylab("Sentiment")
+    + xlab("Date")
+    + theme(axis_line = element_line(color = 'black'))
+    + theme_bw()  
+    + theme(panel_background = element_rect(),
+            plot_background = element_rect(),
+            strip_background = element_rect(),
+            panel_grid_major_x = element_line(size=0.1, color = 'grey',linetype='-'),
+            panel_grid_minor_x = element_rect(),
+            panel_spacing = float(1),
+            axis_line = element_line(size=0.6, color = 'black'),
+            legend_key = element_rect(fill='white'),
+            axis_ticks = element_line(color='black'),
+            figure_size = (16,8),
+            )
+    + theme(axis_line = element_line(color = 'black'))
+)
+
+print(pol_graph)
+
+##csv파일로 출력
+n_pol_df.to_csv(r'C:\GOD\programming\NLP\DecoSentA\SSAbased_Trend\result.csv')
+
+os.startfile(r'C:\GOD\programming\NLP\DecoSentA\SSAbased_Trend\result.csv')
